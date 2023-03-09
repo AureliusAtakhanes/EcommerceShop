77a49a58aab07327a20f139f6e866f6e9a746974
Aurelius Atakhanes <atahanbapaev@gmail.com>
2023-03-09 13:00:31 +0600

    added search component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,25 @@ import styles from '../../styles/Header.module.css';
 import { ROUTES } from '../../utils/routes';
 
 import LOGO from '../../images/logo.svg';
-import AVATAR from '../../images/avatar.jpg';
+import AVATAR from '../../images/login.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleForm } from '../../features/user/userSlice';
+import { useGetProductsQuery } from '../../features/api/apiSlice';
 
 const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const [searchValue, setSearchValue] = useState('')
+
     const { currentUser } = useSelector(({ user }) => user)
 
     const [values, setValues] = useState({
-        name: 'Гость',
+        name: 'Войти',
         avatar: AVATAR
     })
+
+    const { data, isLoading } = useGetProductsQuery({ title: searchValue })
 
     useEffect(() => {
         if (!currentUser) return;
@@ -32,6 +37,10 @@ const Header = () => {
         } else {
             navigate(ROUTES.PROFILE)
         }
+    }
+
+    const handleSearch = ({ target: { value } }) => {
+        setSearchValue(value)
     }
 
     return (
@@ -64,11 +73,36 @@ const Header = () => {
                             name='search'
                             placeholder='search'
                             autoComplete='off'
-                            onChange={() => { }}
-                            value=''
+                            onChange={handleSearch}
+                            value={searchValue}
                         />
                     </div>
-                    {/* <div className={styles.box}></div> */}
+
+                    {searchValue && (
+                        <div className={styles.box}>
+                            {isLoading
+                                ? "Loading"
+                                : !data.length
+                                    ? "No results"
+                                    : data.map(({ title, images, id }) => {
+                                        return (
+                                            <Link
+                                                key={id}
+                                                onClick={() => setSearchValue("")}
+                                                className={styles.item}
+                                                to={`/products/${id}`}
+                                            >
+                                                <div
+                                                    className={styles.image}
+                                                    style={{ backgroundImage: `url(${images[0]})` }}
+                                                />
+                                                <div className={styles.title}>{title}</div>
+                                            </Link>
+                                        );
+                                    })}
+                        </div>
+                    )}
+
                 </form>
 
                 <div className={styles.account}>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,15 @@
 export function shuffle(arr) {
     return [...arr].sort(() => 0.5 - Math.random());
 }
+
+export const buildUrl = (url, params) => {
+    let urlWithParams = url;
+
+    Object.entries(params).forEach(([key, value], i) => {
+        const sign = !i ? '?' : '&';
+
+        urlWithParams += `${sign}${key}=${value}`
+    })
+
+    return urlWithParams;
+}
